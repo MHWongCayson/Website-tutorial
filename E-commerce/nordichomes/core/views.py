dd41bbb00f6850144b21ab76d1f14d2b455fbe40
MHWongCayson <110019862+MHWongCayson@users.noreply.github.com>
2024-01-19 15:53:33 +0800

    Add files via upload

---
+++ E-commerce/nordichomes/core/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+from product.models import Product, Category
+
+def frontpage(request):
+    products= Product.objects.all()[0:8]
+    return render(request, "core/frontpage.html", {'products':products})
+
+def shop(request):
+    categories = Category.objects.all()
+    products = Product.objects.all()
+
+    context =  {
+        'categories': categories,
+        'products':products
+    }
+    return render(request, "core/shop.html", context)
